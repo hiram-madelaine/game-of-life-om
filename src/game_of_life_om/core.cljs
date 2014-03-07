2fa5dbbe6b2999ce3cb5716488fb2a94ad77db14
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2014-03-07 18:15:43 +0100

    Add click on the board to add/remove cell

--- src/game_of_life_om/core.cljs
+++ src/game_of_life_om/core.cljs
@@ -1,8 +1,10 @@
 (ns game-of-life-om.core
-  (:require-macros [dommy.macros :refer [node sel sel1]])
+  (:require-macros [dommy.macros :refer [node sel sel1]]
+                   [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [dommy.core :as dommy]))
+            [dommy.core :as dommy]
+            [cljs.core.async :refer [chan put! <!]]))
 
 (enable-console-print!)
 
@@ -30,26 +32,39 @@
 (defn ->loc
   "Give the absolute position in one dimension."
   [[y x]]
-  [(+ x (* size y))])
+  (+ x (* size y)))
 
-
+(defn ->cell
+  "Give the cell coord given the absolute position"
+  [l]
+  ((juxt quot rem) l size))
 
 ;;;;;;;;;;;;;;;; States ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def cells-state (atom #{[2 0] [2 1] [2 2] [1 2] [0 1]}))
+(def organic #{[14 13] [14 14] [16 13] [16 14] [15 15] [15 12] [13 15]})
+(def organic-2 #{[14 13] [14 14] [15 15] [13 15] [15 12] [16 13] [16 14] [17 12]})
+(def T->blinker #{[11 12] [12 12] [13 12] [12 13]})
+(def blinker #{[12 12] [14 10] [11 6] [14 8] [10 6] [14 9] [8 8] [11 12] [8 9] [10 12] [8 10] [12 6]})
+(def glider  #{[2 0] [2 1] [2 2] [1 2] [0 1]})
+
+(def cells-state (atom #{}))
 
 
-(def empty-board (vec (repeat (* size size) nil)))
+(def empty-board (vec (for [i (range 0 (* size size))]
+                               [i false])))
 
 
 (defn populate
   "Generate the new board, based on the position of the cells."
   [gen]
   (reduce
-   (fn [b c] (assoc-in b (->loc c) :x))
+   (fn [b c]
+     (let [l (->loc c)]
+       (if (<= 0 l (dec (* size size)))
+         (assoc-in b [l 1] true)
+         b)))
    empty-board gen))
 
-(def board (atom (populate @cells-state)))
 
 
 (defn make-step!
@@ -63,24 +78,51 @@
 
 (defn cell-view
   "Display of a single cell."
-  [cell owner]
-  (om/component
-   (let [style (when cell "live")]
-     (dom/div #js {:className style} ""))))
-
-
+  [[id s] owner]
+  (reify
+   om/IRenderState
+    (render-state [_ {:as state :keys [chan]}]
+                  (let [style (when s "live")]
+                    (dom/div #js {:className style
+                                  :id id
+                                  :onClick #(put! chan id)} "")))))
 (defn board-view
   "Display of the entire board."
   [app owner]
-  (om/component
-   (apply dom/div #js {:id "visualizer"}
-          (om/build-all cell-view app))))
+  (reify
+    om/IRenderState
+    (render-state [_ state]
+                  (apply dom/div #js {:id "visualizer"}
+                         (om/build-all cell-view app {:state state})))))
+
+
+(defn app-view
+  "Display of the entire board."
+  [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+                {:chan (chan)})
+    om/IWillMount
+    (will-mount [_]
+                (let [chan (om/get-state owner [:chan])]
+                 (go (loop []
+                       (let [id (<! chan)]
+                         (om/transact! app (fn [cells] (let [cell (->cell id)]
+                                                        (if (contains? cells cell)
+                                                          (disj cells cell)
+                                                          (conj cells (->cell id)))))))
+                       (recur)))))
+    om/IRenderState
+    (render-state [_ state]
+     (om/build board-view app {:fn populate
+                               :state state}))))
+
 
 (om/root
- board-view
+ app-view
  cells-state
- {:target (. js/document (getElementById "board"))
-  :fn populate})
+ {:target (. js/document (getElementById "board"))})
 
 ;;;;;;;;;;; Mark ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -99,8 +141,8 @@
 #_(js/setInterval make-step! 500)
 
 
-
-
+#_(prn @cells-state)
+#_(reset! cells-state organic)
 
 (defn animate []
   (.requestAnimationFrame js/window animate)
