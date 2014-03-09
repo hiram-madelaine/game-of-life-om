23fb021d6f8d544e0645d2bcca9e0ede009ffe4d
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2014-03-09 12:49:36 +0100

    Better addition of new cells

--- src/game_of_life_om/core.cljs
+++ src/game_of_life_om/core.cljs
@@ -46,7 +46,8 @@
 (def T->blinker #{[11 12] [12 12] [13 12] [12 13]})
 (def blinker #{[12 12] [14 10] [11 6] [14 8] [10 6] [14 9] [8 8] [11 12] [8 9] [10 12] [8 10] [12 6]})
 (def glider  #{[2 0] [2 1] [2 2] [1 2] [0 1]})
-
+(def hand-circles #{[5 11] [6 12] [8 13] [11 15] [13 16] [14 19] [18 22] [4 11] [5 10] [6 13] [8 14] [11 14] [12 18] [13 19] [14 16] [17 22] [4 12] [8 15] [11 13] [12 19] [15 16] [16 22] [4 13] [8 16] [12 12] [6 8] [9 17] [6 9] [11 18] [10 18] [12 15] [20 21] [22 19] [11 8] [12 8] [15 11] [5 19] [10 8] [13 8] [14 11] [5 18] [7 20] [8 6] [10 5] [11 6] [13 11] [14 8] [11 5] [12 11] [15 8] [9 10] [11 12] [12 4] [14 6] [4 14] [6 16] [9 9] [13 4] [15 6] [4 15] [6 17] [8 10] [10 9] [12 6] [14 4] [4 16] [6 14] [8 11] [13 6] [4 17] [6 15] [8 12] [15 3] [16 3] [17 4] [19 6] [20 6] [21 7] [15 2] [16 4] [18 6] [19 5] [22 9] [17 6] [16 6] [18 4] [21 8] [11 4] [17 8] [18 9] [19 10] [16 8] [19 9] [18 8] [16 11] [17 12] [20 14] [9 21] [20 15] [22 17] [10 21] [17 14] [23 15] [17 13] [19 11] [14 22] [17 16] [18 17] [23 13] [15 22] [16 16] [17 15] [18 18] [19 17] [20 11] [23 12] [12 22] [17 18] [19 16] [20 12] [22 10] [16 18] [20 13]} )
+(def space-invader #{[11 11] [12 11] [13 11] [14 11] [15 11] [11 12] [15 12] [11 13] [12 13] [13 13] [14 13] [15 13]})
 (def cells-state (atom #{}))
 
 
@@ -75,7 +76,24 @@
 
 ;;;;;;;;;;;;;;; Events handlers ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defmulti board-event (fn [[e _] app owner] e))
 
+
+(defmethod board-event :hover
+  [[_ id] app owner]
+  (when (om/get-state owner [:capture])
+    (om/update-state! owner [:captured] #(conj % id))))
+
+
+(defmethod board-event :click
+  [[_ id] app owner]
+  (om/update-state! owner [:captured] #(conj % id))
+  (when (om/get-state owner [:capture])
+    (let [new-cells (map ->cell (om/get-state owner [:captured]))
+          _ (prn new-cells)]
+      (om/transact! app #(set (concat % new-cells))))
+    (om/set-state! owner [:captured] #{}))
+  (om/update-state! owner [:capture] not))
 
 ;;;;;;;;;;;;;; Om components ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -84,8 +102,9 @@
   [[id s] owner]
   (reify
    om/IRenderState
-    (render-state [_ {:as state :keys [chan]}]
-                  (let [style (when s "live")]
+    (render-state [_ {:as state :keys [chan captured]}]
+                  (let [style (when s "live")
+                        style (if (contains? captured id) (str style " captured" ) style)]
                     (dom/div #js {:className style
                                   :id id
                                   :onMouseOver #(put! chan [:hover id])
@@ -114,17 +133,14 @@
     om/IInitState
     (init-state [_]
                 {:chan (chan)
-                 :capture false})
+                 :capture false
+                 :captured #{}})
     om/IWillMount
     (will-mount [_]
                 (let [chan (om/get-state owner [:chan])]
                  (go (loop []
                        (let [[e id] (<! chan)]
-                         (condp = e
-                           :click (om/update-state! owner :capture not)
-                           :hover )
-                         (when (om/get-state owner [:capture])
-                           (capture! app id)))
+                         (board-event [e id] app owner ))
                        (recur)))))
     om/IRenderState
     (render-state [_ state]
@@ -151,11 +167,11 @@
 
 ;;;;;;;;;;;;;;;;;;; Animate ! ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-#_(js/setInterval make-step! 200)
+(js/setInterval make-step! 200)
 
 
-#_(prn @cells-state)
-#_(reset! cells-state organic)
+(prn @cells-state)
+#_(reset! cells-state hand-circles )
 
 (defn animate []
   (.requestAnimationFrame js/window animate)
