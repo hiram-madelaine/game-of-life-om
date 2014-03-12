2d43361a80a1ab436fa522cc1e69ae89fd99d258
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2014-03-12 11:02:47 +0100

    Om Version 5.0.2

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.5.2-SNAPSHOT"]
+                 [om "0.5.2"]
                  [prismatic/dommy "0.1.2"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
